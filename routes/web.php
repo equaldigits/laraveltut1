d13059a1f2c90d80df92caa035a7e1f79f9d41c1
equaldigits <rui.nz@hotmail.com>
2018-02-13 17:03:18 +0000

    Innitial Commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use App\Task;
+ //importar a classe para enunciar a função Task
+
+Route::get('/tasks', function () {
+    //$tasks = DB::table('tasks')->get();
+    //$tasks = DB::table('tasks')->latest()->get();
+    $tasks = Task::all();
+    //return $tasks;
+
+    return view('tasks.index', compact('tasks'));
+});
+
+
+Route::get('/tasks/{task}', function ($id) {
+    //$task = DB::table('tasks')->find($id);
+
+    $task = Task::find($id);
+
+
+    return view('tasks.show', compact('task'));
+});
